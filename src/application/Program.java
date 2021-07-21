68198d6822763694eedd2c5ccffc2fcecf81601d
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-07-21 14:53:41 -0300

    programa finalizado

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,15 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import model.entities.Product;
+import model.service.CalculationService;
 
 /*
   Uma empresa de consultoria deseja avaliar a performance de produtos,
@@ -18,7 +26,26 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
+		List<Product> list = new ArrayList<Product>();
 		
+		String strPath = "C:\\temp\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(strPath))) {
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
+			}
+			
+			Product x = CalculationService.max(list);
+			System.out.println("Most expensive:");
+			System.out.println(x);
+			
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
 		
 		sc.close();
 		
